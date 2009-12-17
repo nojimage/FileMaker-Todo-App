c77864807c14cb99ad90edfeda45d6b45fd6c406
nojimage <nojimage@gmail.com>
2009-12-17 22:03:33 +0900

    FMCakeMix base

---
+++ app/config/database.php
@@ -0,0 +1,31 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+	    'driver' => 'fmcakemix',
+	    'persistent' => false,
+	    'dataSourceType' => 'FMPro7',
+	    'scheme' => 'http',
+	    'port' => 80,
+	    'host' => '127.0.0.1',
+	    'login' => 'Admin',
+	    'password' => 'admin',
+	    'database' => 'To Do リスト',
+	    'prefix' => '',
+	); 
+
+	/*
+	var $test = array(
+		'driver' => 'fmcakemix',
+	    'persistent' => false,
+	    'dataSourceType' => 'FMPro7',
+	    'scheme' => 'http',
+	    'port' => 80,
+	    'host' => '127.0.0.1',
+	    'login' => 'Admin',
+	    'password' => '',
+	    'database' => 'DriverUnitTesting',
+	    'prefix' => '',
+	);
+	*/
+}
